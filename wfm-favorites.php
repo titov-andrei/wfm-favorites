fef2ab3ba92d7875cd5801119f986fd21d334001
titov-andrei <titov_andrei@mail.ru>
2020-10-14 10:23:27 +0300

    Update wfm-favorites.php

--- wfm-favorites.php
+++ wfm-favorites.php
@@ -4,8 +4,8 @@
 Plugin Name: Добавление статей в Избранное
 */
 
-add_filter( 'the_title', 'wfm_favorites' );
+add_filter( 'the_content', 'wfm_favorites_content' );
 
-function wfm_favorites($title) {
-  return $title;
+function wfm_favorites_content($content) {
+  return '<p class="wfm-favorites-link"><a href=""></a></p>' . $content;
 }
